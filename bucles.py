fcd65228b45cc6ab57f3d9f1d7171a24dec7ab55
Kelvin Silvestre <skell14@hotmail.com>
2020-10-31 10:11:38 -0400

    Ciclo for

--- bucles.py
+++ bucles.py
@@ -7,5 +7,7 @@ def run():
         print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2)) 
         contador = contador + 1
         potencia_2 = 2**contador
+
+        
 if __name__ == '__main__':
     run()
---
+++ for.py
@@ -0,0 +1,7 @@
+
+
+for contador in range(1001):
+    print(contador)
+
+for i in range(10):
+    print(5 * i)
